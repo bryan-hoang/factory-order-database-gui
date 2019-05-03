7a8612612cae90cc5c7efe9dfd671308ad878229
ameshkahloon <44043757+ameshkahloon@users.noreply.github.com>
2019-05-02 21:35:29 -0400

    Add files via upload

---
+++ Window1.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Order_System_UI
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        public Window1(string SellerName, string TruckingCompanyName, string Quality, string Weight, string Price, string NumberOfBags, string FreightCharges, string ShipmentNumber, string DateOfArrival, string TotalCost)
+        {
+            InitializeComponent();
+
+            sellerName.Text = SellerName;
+            truckingCompanyName.Text = TruckingCompanyName;
+            quality.Text = Quality;
+            weight.Text = Weight;
+            price.Text = Price;
+            numberOfBags.Text = NumberOfBags;
+            freightCharges.Text = FreightCharges;
+            shipmentNumber.Text = ShipmentNumber;
+            dateOfArrival.Text = DateOfArrival;
+            totalCost.Text = TotalCost;
+
+        }// end constructor method
+
+        // button to go back to make corrections
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+                    
+
+        }// end method
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }// end method
+
+    }
+}
