d9f1cc36720ba6f7104acba20c67be0bb883bafc
Bryan Hoang <Bryan.C.Hoang@gmail.com>
2019-05-06 23:20:38 -0400

    Refactor

--- CottonOilFactory.OrderGUI.Data/Properties/AssemblyInfo.cs
+++ CottonOilFactory.OrderGUI.Data/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("KahloonCottonOilFactory.OrderGUI")]
+[assembly: AssemblyTitle("CottonOilFactory.OrderGUI")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("KahloonCottonOilFactory.OrderGUI")]
+[assembly: AssemblyProduct("CottonOilFactory.OrderGUI")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
---
+++ CottonOilFactory.OrderGUI.UI/SalesDataInputWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Windows;
+using CottonOilFactory.OrderGUI.Data;
+
+namespace CottonOilFactory.OrderGUI.UI
+{
+
+    /// <summary>
+    /// Interaction logic for Window2.xaml
+    /// </summary>
+    public partial class SalesDataInputWindow : Window
+    {
+
+        private SalesData salesData;
+
+        public SalesDataInputWindow()
+        {
+            InitializeComponent();
+            SalesData salesData = new SalesData();
+            this.salesData = salesData;
+            this.DataContext = salesData;
+        }
+
+        public SalesDataInputWindow(SalesData salesData)
+        {
+            InitializeComponent();
+            this.salesData = salesData;
+            this.DataContext = salesData;
+        }
+
+        private void GoBackButton_Click(object sender, RoutedEventArgs e)
+        {
+            var mainWindow = new MainWindow();
+            this.Close();
+            mainWindow.Show();
+        }
+
+        private void ConfirmButton_Click(object sender, RoutedEventArgs e)
+        {
+            var salesDataConfirmWindow = new SalesDataConfirmWindow(salesData);
+        }
+    }
+}
